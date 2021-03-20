6f783095fc4461a2f64a902cd4e67c0b31ee29cc
unknown <aelise17264@gmail.com>
2021-03-20 15:50:14 -0600

    manipulated attributes w/ jQuery

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 $("h1").addClass("big-title").text("Goodbye");
 
-$("button").css("background-color", "blue").css("color", "white").html("<em>This Button</em>")
+$("button").css("background-color", "blue").css("color", "white").html("<em>This Button</em>");
+
+console.log($("img").attr("src"))
+$("a").attr("href", "https://www.yahoo.com")
