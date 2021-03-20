c0888aa44e9a7642fe707c5aa1ab99a7503a5e07
unknown <aelise17264@gmail.com>
2021-03-20 17:21:03 -0600

    game set up, blocks will flash, working out kinks with playing sounds

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # jQuery
+
+## After going through some introductory exercises, created a game modled after the Simon game (hello 90's babies).
--- index.js
+++ index.js
@@ -1,28 +1,51 @@
-$("h1").addClass("big-title").text("Goodbye");
+// alert("Game On!")
+var buttonColors = ["red", "blue", "green", "yellow"];
 
-$("button").css("background-color", "blue").css("color", "white").html("<em>This Button</em>");
+var gamePattern = [];
 
-console.log($("img").attr("src"))
-$("a").attr("href", "https://www.yahoo.com")
 
-$("h1").click(function(){
-    $("h1").css("color", "purple")
-})
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+    // return randomNumber
+    var randomChosenColor = buttonColors[randomNumber];
+// console.log(randomChosenColor)
+    gamePattern.push(randomChosenColor);
+// console.log(gamePattern)
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    var boxSound = new Audio("sounds/" + randomChosenColor + ".mp3");
+    boxSound.play();
 
-$("button").click(function(){
-    $("h1").slideUp().slideDown()
-})
+}
+nextSequence()
 
-$(document).keypress(function(event){
-    console.log(event.key)
-    $("h1").text(event.key)
-})
 
-$("h1").on("mouseenter", function(){
-    $("h1").css("color", "red")
-})
-$("h1").on("mouseout", function(){
-    $("h1").css("color", "yellow")
-})
 
-$("h1").before("<button>New</button>")
+
+// $("h1").addClass("big-title").text("Goodbye");
+
+// $("button").css("background-color", "blue").css("color", "white").html("<em>This Button</em>");
+
+// console.log($("img").attr("src"))
+// $("a").attr("href", "https://www.yahoo.com")
+
+// $("h1").click(function(){
+//     $("h1").css("color", "purple")
+// })
+
+// $("button").click(function(){
+//     $("h1").slideUp().slideDown()
+// })
+
+// $(document).keypress(function(event){
+//     console.log(event.key)
+//     $("h1").text(event.key)
+// })
+
+// $("h1").on("mouseenter", function(){
+//     $("h1").css("color", "red")
+// })
+// $("h1").on("mouseout", function(){
+//     $("h1").css("color", "yellow")
+// })
+
+// $("h1").before("<button>New</button>")
